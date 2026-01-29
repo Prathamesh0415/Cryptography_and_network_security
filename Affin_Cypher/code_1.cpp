19f0c76f09d5c0bf33589606ae44d2103742b957
Prathamesh Ghormare <prathameshghormare@gmail.com>
2026-01-29 10:59:18 +0530

    understand affin cipher

---
+++ Affin_Cypher/code_1.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+#include<string>
+#include<vector>
+
+using namespace std;
+
+//choose a and b yourself also remember a should be a coprime with 26
+// a cant be even and a cant be 13
+//also a and b are between 0 to 25
+const int a  = 3;
+//b can be anything less than 25 and it represents the shift
+const int b = 7;
+
+//ENCRYPTION
+//Formula is (ax + b) % m
+// which becomes (3x + 7) % 26
+
+//DECRYPTION
+//in decyption the formula becomes a^-1(x - b) % 26
+//This follows the prooperty a^-1*a % 26 == 1 so bascially a^-1 . a == 27
+//in this case since a == 3 Therfore a^-1 == 9 since 9 . 3 == 27
+
+string ENCRYPT(string a, vector<char>mapper){
+    string temp = "";
+    for(int i = 0; i < a.size(); i++){
+        temp += 
+    }
+}
+
+int main(){
+    vector<char>vec;
+    
+    for(int i = 'a'; i <= 'z'; i++){
+        vec.push_back(i);
+    }
+}
